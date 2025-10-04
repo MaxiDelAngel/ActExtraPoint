a7b155a91030ad5b9a1ba081d7fd3c068c972a47
roberto molina <jose.molina@iest.edu.mx>
2025-10-03 18:26:32 -0600

    first commit

---
+++ ActExtraPoint/ActExtraPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActExtraPointApp.swift
+//  ActExtraPoint
+//
+//  Created by WIN603 on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ActExtraPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
